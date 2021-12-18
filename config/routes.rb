0b77f02517feafeb0be75fbfa93c2741afa1b8c9
Allen Cheng <allen.cheng1984@gmail.com>
2021-12-18 14:50:29 +0800

    team member done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
-  get 'dashboard/index'
+
+  get '/team_members', to: 'dashboard#team_members'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
