08dd2f5860f210d2710397a48e32af79096151fe
Allen Cheng <allen.cheng1984@gmail.com>
2021-12-18 08:48:30 +0800

    close turbolink and set partial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,13 +4,13 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
 
 Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
 import $ from 'jquery';
 import Popper from 'popper.js';
