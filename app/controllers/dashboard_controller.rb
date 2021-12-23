dd6ee59b1a5ca662fd89cb4fd3736d2f720fa629
Allen Cheng <allen.cheng1984@gmail.com>
2021-12-23 09:30:18 +0800

    add dashboard/index data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,10 @@ class DashboardController < ApplicationController
 
   def index
     @dimension = params[:dimension] || 'year'
-    @date_range = params[:date_range]
+    @date_range = params[:date_range] || nil
+
+    @orders = set_index_orders(@dimension, @date_range)
+    @orders_payments_sum = @orders.map{|order| order.total_payment_value }.sum
   end
 
   def team_members
@@ -17,4 +20,18 @@ class DashboardController < ApplicationController
 
   def dataset_info
   end
+
+  private
+
+    def set_index_orders(dimension, date_range)
+      orders = Order.where(order_purchase_year: '2018')         if dimension == 'year'  && !date_range
+      orders = Order.where(order_purchase_year_month: '201810') if dimension == 'month' && !date_range
+      orders = Order.where(order_purchase_yearweek: '201842')   if dimension == 'week'  && !date_range
+
+      orders = Order.where(order_purchase_year: date_range)       if dimension == 'year'  && date_range
+      orders = Order.where(order_purchase_year_month: date_range) if dimension == 'month' && date_range
+      orders = Order.where(order_purchase_yearweek: date_range)   if dimension == 'week'  && date_range
+
+      orders
+    end
 end
