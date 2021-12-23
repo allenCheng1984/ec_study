45dddd5eebb73d3bc054c3ac30aed8b0547c2c9f
Allen Cheng <allen.cheng1984@gmail.com>
2021-12-23 09:57:12 +0800

    fix bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   layout 'dashboard'
 
   def index
-    @dimension = params[:dimension] || 'year'
+    @dimension = params[:dimension] || 'month'
     @date_range = params[:date_range] || nil
 
     @orders = set_index_orders(@dimension, @date_range)
@@ -24,14 +24,14 @@ class DashboardController < ApplicationController
   private
 
     def set_index_orders(dimension, date_range)
-      orders = Order.where(order_purchase_year: '2018')         if dimension == 'year'  && !date_range
-      orders = Order.where(order_purchase_year_month: '201810') if dimension == 'month' && !date_range
-      orders = Order.where(order_purchase_yearweek: '201842')   if dimension == 'week'  && !date_range
+      return Order.where(order_purchase_year: '2017')         if dimension == 'year'  && !date_range
+      return Order.where(order_purchase_year_month: '201712') if dimension == 'month' && !date_range
+      return Order.where(order_purchase_yearweek: '201748')   if dimension == 'week'  && !date_range
 
-      orders = Order.where(order_purchase_year: date_range)       if dimension == 'year'  && date_range
-      orders = Order.where(order_purchase_year_month: date_range) if dimension == 'month' && date_range
-      orders = Order.where(order_purchase_yearweek: date_range)   if dimension == 'week'  && date_range
+      return Order.where(order_purchase_year: date_range)       if dimension == 'year'  && date_range
+      return Order.where(order_purchase_year_month: date_range) if dimension == 'month' && date_range
+      return Order.where(order_purchase_yearweek: date_range)   if dimension == 'week'  && date_range
 
-      orders
+      return []
     end
 end
